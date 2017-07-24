873179e9a2d2b1c14fbed1c83b059c8f222344c7
HuangJ <huangays@gmail.com>
2017-07-25 00:14:04 +0800

    【第八次修改】主页轮播图待实现

--- app/src/main/java/com/weifupro/fragment/HomeFragment.java
+++ app/src/main/java/com/weifupro/fragment/HomeFragment.java
@@ -1,10 +1,14 @@
 package com.weifupro.fragment;
 
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
+import com.squareup.picasso.Picasso;
 import com.weifupro.R;
 import com.weifupro.bean.AnnImageResult;
 import com.weifupro.bean.AnnImages;
@@ -29,7 +33,7 @@ public class HomeFragment extends BaseFragment {
     private ViewPager mHomeFragmentviewPager;
     private LinearLayout mLinearLayout;
     private int count = 0;//轮播图当前下标
-    private View view;//根布局
+    private List<View> views;
     public final int GetImags = 1014;//获取广告图返回码
     private final int AnnFaild = 1011;//获取广告图失败返回码
     private Timer mTimer;//计时器
@@ -46,10 +50,17 @@ public class HomeFragment extends BaseFragment {
         super.init(view);
         mHomeFragmentviewPager = (ViewPager) view.findViewById(R.id.fragment_img_viewpager);
         mLinearLayout = (LinearLayout) view.findViewById(R.id.fragment_point_subscript);
+        if (views == null){
+            views = new ArrayList<>();
+        }
+        HMViewpage hmViewpageAdapter = new HMViewpage();
+        mHomeFragmentviewPager.setAdapter(hmViewpageAdapter);//轮播图的适配器;
+        //添加界面滚动监听
+        mHomeFragmentviewPager.addOnPageChangeListener(hmViewpageAdapter);
     }
 
     /**
-     *下载数据
+     *下载轮播图的数据
      */
     @Override
     protected void loadDatas() {
@@ -80,7 +91,7 @@ public class HomeFragment extends BaseFragment {
                 updateBanner(img_List);
                 // 保存到数据库;更新缓存
                 if (img_List.size() > 0){
-                    // 删除缓存
+                    // 删除数据库缓存
                     DataSupport.deleteAll(AnnImages.class);
                     // 更新新的数据
                     DataSupport.saveAll(img_List);
@@ -88,12 +99,64 @@ public class HomeFragment extends BaseFragment {
 
             }
 
-            /**
-             * 根据公告图片地址动态更新界面
-             */
-            private void updateBanner(List<AnnImages> img_List) {
-                Log.d("print", "updateBanner: 更新界面");
-            }
         });
+    }
+    /**
+     * 根据公告图片地址动态更新界面
+     */
+    public void updateBanner(List<AnnImages> img_List) {
+        Log.d("print", "updateBanner: 更新界面");
+        views.clear();//清除view的缓存
+        for (AnnImages images : img_List) {
+            ImageView img = new ImageView(getActivity());
+            img.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            Picasso.with(getActivity())
+                    .load(images.getImgUrl())
+                    .into(img);
+            views.add(img);
+        }
+    }
+    private class HMViewpage extends PagerAdapter implements ViewPager.OnPageChangeListener
+    {
+
+        @Override
+        public int getCount() {
+            return views.size();
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView(views.get(position));//移除图片
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            container.addView(views.get(position));
+            return views.get(position);
+        }
+
+        /**
+         * 滑动监听  动态更改指示下标的选中状态
+         * @param position
+         * @param positionOffset
+         * @param positionOffsetPixels
+         */
+        @Override
+        public void onPageSelected(int position) {
+
+        }
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
     }
 }
